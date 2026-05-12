0e4ad1b883e8b0876c4102f16844eddb63a3bfbd
LionSeopBong <gkstjq309@gmail.com>
2026-05-12 20:26:51 +0900

    #2 Init LoginPage

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoginPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import LoginPage from "@/views/LoginPage.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { LoginPage },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background: black;
+  color: white;
+}
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/PropsChildComponents.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ message }} {{ toChild }}</div>
+</template>
+
+<script>
+export default {
+  name: "PropschildComponents",
+
+  data() {
+    return {
+      message: " 나는 자식컴포넌트",
+    };
+  },
+  props: {
+    toChild: String,
+  },
+};
+</script>
